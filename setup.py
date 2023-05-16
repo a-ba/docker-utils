db001bf2156950faa687798fa8e7160a28b749cf
Anthony Baire <Anthony.Baire@irisa.fr>
2023-05-16 10:17:57 +0200

    version 0.0.24

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ assert sys.version_info[0] > 2, "python2 not supported"
 
 setup(
     name        = "docker-utils-aba",
-    version     = "0.0.23",
+    version     = "0.0.24",
     scripts     = """
         docker-diff
         docker-flatten
